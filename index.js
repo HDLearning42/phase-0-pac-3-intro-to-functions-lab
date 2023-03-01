973ca4480250e36424333e1df46b0a27b75cd1c4
Holli Dawson <hldawson42@gmail.com>
2023-03-01 14:43:48 -0700

    revises the tests

---
+++ index.js
@@ -0,0 +1,28 @@
+const expect = require("expect/lib");
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string){
+    //if string lowercase then print 'I can't hear you'
+if(string === string.toLowerCase()) {
+    return("I can't hear you!")
+} 
+else if(string === string.toUpperCase()) {
+    return("YES INDEED!")
+}
+else if(string === "Let's have dinner together!") {
+    return("I would love to!")
+}
+}
+console.log(sayHiToHeadphonedRoommate("Let's have dinner together!"))
